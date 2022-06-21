978d6260e2ba9f5141a5220cd3bd9199e140422e
devkhishan08 <107935099+devkhishan08@users.noreply.github.com>
2022-06-21 19:42:56 +0530

    Update main.py

--- module1-QR/main.py
+++ module1-QR/main.py
@@ -1,1 +1,9 @@
-print("main")
+import qrcode
+import cv2
+img = qrcode.make('Hello World')
+img.save('hello.png')
+
+img = cv2.imread("hello.png")
+det=cv2.QRCodeDetector()
+val,pts,st_code=det.detectAndDecode(img)
+print(val)
